8527e33fc45a8888828f62e8457c0c703cb5e9ed
_onmac <igorkoh2@gmail.com>
2022-12-23 22:25:08 +0100

    Create initial component Home.js

---
+++ bookrate/src/components/home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Home() {
+
+    const [books, setBooks] = React.useState([]);
+
+  return (
+
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default Home;
