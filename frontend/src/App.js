abcbbde200cb3becf157d2bbc092d0a86bd378a1
_onmac <igorkoh2@gmail.com>
2022-12-26 14:51:27 +0100

    Add JPA and H2 dependencies into project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import Home from './components/home/Home';
 function App() {
   return (
    <>
-    <Home siema />
+    <Home/>
    </>
   );
 }
--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -8,7 +8,7 @@ function Home() {
   return (
 
     <div>
-      <h1>Home</h1>
+      <h1>HELLO WORLD</h1>
     </div>
   );
 }
