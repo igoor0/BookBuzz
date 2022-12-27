1dda3644654c197678bf67544f30d79aa9b1bbe6
_onmac <igorkoh2@gmail.com>
2022-12-27 22:35:08 +0100

    Revert "Delete Frontend in matter of bugfixing"
    This reverts commit 748db297835da1e25dd96094a83f33c80e605267.

---
+++ frontend/src/components/home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Home() {
+
+    const [books, setBooks] = React.useState([]);
+
+  return (
+
+    <div>
+      <h1>HELLO WORLD</h1>
+    </div>
+  );
+}
+
+export default Home;
