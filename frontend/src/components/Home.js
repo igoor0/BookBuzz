17be98b0250cedfe37041fae61a53a07df0c8952
_onmac <igorkoh2@gmail.com>
2022-12-27 21:58:06 +0100

    Fix missing components, start adding UserService

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,20 @@
+Path: frontend/src/components/Home.js
+import React from 'react';
+
+ const Home = () => {
+   return (
+     <div>
+       <h1>Hello World!</h1>
+       <h2>Hello World!</h2>
+       <h3>Hello World!</h3>
+         <h4>Hello World!</h4>
+            <h5>Hello World!</h5>
+                <h6>Hello World!</h6>
+
+
+     </div>
+   );
+   export default Home;
+ };
+
+
