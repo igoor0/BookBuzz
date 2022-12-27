d82f3d34fcea624768d6d665422b52ba116be20c
_onmac <igorkoh2@gmail.com>
2022-12-28 00:27:19 +0100

    Add GetMapping for users

--- src/main/java/pl/BookRate/bookRate/controller/UserController.java
+++ src/main/java/pl/BookRate/bookRate/controller/UserController.java
@@ -1,11 +1,14 @@
 package pl.BookRate.bookRate.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.BookRate.bookRate.model.User;
 import pl.BookRate.bookRate.repository.UserRepository;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -16,6 +19,11 @@ public class UserController {
     @PostMapping("/user")
     User newUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 
 
